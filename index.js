e25996b45d98538827bf9888bc8cdcbe82674208
Lexi Ives <lexives96@gmail.com>
2021-03-15 12:21:32 -0500

    move some code from index.js to server.js

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-// import and download dependencies
-const express = require("express");   // api calls
-const dotenv = require("dotenv");     // environment variables
-const MongoDBConnect = require("./app/db/MongoDBConnect")
+/**
+ * index.js is the main entrypoint of the app. From here we will set up
+ * environment variables and start the server from the server.js file.
+ */
 
-// configure dotenv -- SHOULD HAPPEN AS EARLY AS POSSIBLE
+// Configure dotenv so we can get environment variables
+// (This should happen as early as possible!)
+const dotenv = require("dotenv");
 dotenv.config()
-const PORT = process.env.PORT;
-const APP_SECRET = process.env.APP_SECRET;
 
-// connect to MongoDB
-MongoDBConnect();
-
-// inititalize the app & set up express
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// add handlers for each path
-app.get("/", (req, res) => {
-    return res.send(APP_SECRET);
-  });
-
-// Run the app on the configured port
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+require('./server/server')
--- server/server.js
+++ server/server.js
@@ -1,1 +1,22 @@
-// TODO: move things from index.js here
+const express = require("express");   // api framework
+const MongoDBConnect = require("../app/db/MongoDBConnect") // mongoDB
+
+// Get environment variables
+const PORT = process.env.PORT;
+const APP_SECRET = process.env.APP_SECRET;
+
+// Connect to MongoDB
+MongoDBConnect();
+
+// Inititalize the app & set up express
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// add handlers for each path
+app.get("/", (req, res) => {
+    return res.send(APP_SECRET);
+  });
+
+  // Run the app on the configured port
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
